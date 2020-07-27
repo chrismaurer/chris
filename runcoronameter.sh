26ae9a1a0141112d0c9ae89bdf1546f20270278d
chrismaurer <chris.maurer@tradingtechnologies.com>
2020-07-27 13:42:58 +0900

    update paths again

--- runcoronameter.sh
+++ runcoronameter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
-export PATH="$PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi"
-export NODE_PATH="$NODE_PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi"
+export PATH="$PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi:/usr/local/chris"
+export NODE_PATH="$NODE_PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi:/usr/local/chris"
 cd /usr/local/chris
 /usr/bin/node /usr/local/chris/coronameter.js
 /bin/echo "coronameter.js is running"
