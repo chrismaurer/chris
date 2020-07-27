b5a6fc750e7752eae0f3c9cf99e5f312f8d5d7d9
chrismaurer <chris.maurer@tradingtechnologies.com>
2020-07-27 21:48:03 +0900

    Add date stamp to output

--- runcoronameter.sh
+++ runcoronameter.sh
@@ -4,4 +4,4 @@ export PATH="$PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi:/usr/local/chr
 export NODE_PATH="$NODE_PATH:/usr/bin:/opt/nodejs/lib/node_modules:/home/pi:/usr/local/chris"
 cd /usr/local/chris
 /usr/bin/node /usr/local/chris/coronameter.js
-/bin/echo "coronameter.js is running"
+/bin/echo "`date` coronameter.js is running"
